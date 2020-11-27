519d1ee7e02bddd0c4f7e03f143d8d8146ba3cde
Ezarsatria12 <Ezar3004>
2020-11-27 21:21:48 +0700

    kk4a

---
+++ app/src/main/java/com/ezar/satria/myrecyclerview/Hero.kt
@@ -0,0 +1,11 @@
+package com.ezar.satria.myrecyclerview
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
